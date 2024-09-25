caac6cf5d4cc9eab3baa4b09cf58eebabd2a4940
Leandro Souza <leandro2005souza@gmail.com>
2024-09-25 20:44:52 -0300

    update aside

---
+++ src/components/GameLogo/index.jsx
@@ -0,0 +1,8 @@
+import logoHitRace from '../../images/logo_hitrace.svg'
+import './style.css'
+
+export const GameLogo = () => {
+    return (
+        <img src={logoHitRace} alt="Logo HitRace" className='logo-game' />
+    )
+}
